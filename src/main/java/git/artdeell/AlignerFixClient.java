6af1065f5606a21e47dbd363c446aa60e350fff4
youssefsaad121000-crypto <youssefsaad121000@gmail.com>
2026-07-23 22:48:40 +0300

    Update AlignerFixClient.java

--- src/main/java/git/artdeell/AlignerFixClient.java
+++ src/main/java/git/artdeell/AlignerFixClient.java
@@ -1,16 +1,11 @@
 package git.artdeell;
 
 import net.fabricmc.api.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AlignerFixClient implements ClientModInitializer {
 
-    public static final String MOD_ID = "alignerfix";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
     @Override
     public void onInitializeClient() {
-        LOGGER.info("Aligner Fix loaded!");
+        System.out.println("[AlignerFix] Loaded!");
     }
 }
